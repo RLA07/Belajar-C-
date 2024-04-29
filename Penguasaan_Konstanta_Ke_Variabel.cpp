9308ea91e936b685a2cc8965fa43173dc95ccf47
RLA07 <103559445+RLA07@users.noreply.github.com>
2024-04-29 16:06:38 +0700

    Update and rename contoh.cpp to Penguasaan_Konstanta_Ke_Variabel.cpp

--- Penguasaan_Konstanta_Ke_Variabel.cpp
+++ Penguasaan_Konstanta_Ke_Variabel.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()  {
 	char kar ;	      // Pendefinisian variabel kar
-    kar ='A';	      //  Penugasan ' A 'ke variabel kar
+    kar ='A';	      //  Penugasan 'A' ke variabel kar
     system("cls");
     cout  << "isi kar = " << kar << '\n';
 	kar = 66 ;	      // Penugasan nilai ASCII ke kar
