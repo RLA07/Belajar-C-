c4406ebeca0521fb6e72abbeb86de81cd8f356ae
RLA07 <103559445+RLA07@users.noreply.github.com>
2024-04-29 16:01:44 +0700

    Update contoh.cpp

--- contoh.cpp
+++ contoh.cpp
@@ -1,11 +1,5 @@
-//*----------------------------------------------------------*
-//*                 pchar.cpp                                *
-//*                                                          *
-//*    contoh pendefenisian variabel bertipe char dan        *
-//*      dan penugasan konstanta char ke variabel            *
-//*----------------------------------------------------------*
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 #include <conio.h>
 using namespace std;
 
@@ -16,5 +10,5 @@ int main()  {
     cout  << "isi kar = " << kar << '\n';
 	kar = 66 ;	      // Penugasan nilai ASCII ke kar
 	cout << "Isi kar = " << kar << '\n';
-    //getche();
+    getche();
   }
